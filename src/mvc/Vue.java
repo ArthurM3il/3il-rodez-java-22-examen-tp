c94b34e62d3dd38499c9b4916b205fb59e4a97c6
ArthurM3il <morenoa@3il.fr>
2024-04-05 15:21:02 +0200

    Refacto ajout de quelques composants à la vue

--- src/mvc/Vue.java
+++ src/mvc/Vue.java
@@ -1,7 +1,22 @@
 package mvc;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Vue extends JPanel {
+    private JPanel panelAjoutTache;
+    private TextField idTextField;
 
+    public Vue(){
+        panelAjoutTache = new JPanel();
+        idTextField = new TextField();
+        panelAjoutTache.setSize(new Dimension(1920,1080));
+        panelAjoutTache.add(idTextField);
+    }
+
+    public void actualiserPanel(JPanel panel){
+        for(Component c : panel.getComponents()){
+            if(c instanceof TextField) ((TextField) c).setText("");
+        }
+    }
 }
