acabe50af5f804c79fd3ed7272ece3518309c25c
ArthurM3il <morenoa@3il.fr>
2024-04-05 14:35:41 +0200

    Add creation du Controleur

---
+++ src/Controleur.java
@@ -0,0 +1,8 @@
+public class Controleur {
+    private Vue vue;
+    private Modele modele;
+    public Controleur(Vue vue, Modele modele){
+        this.vue = vue;
+        this.modele = modele;
+    }
+}
